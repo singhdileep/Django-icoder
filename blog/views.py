3e935a81edb9e749b07412a906fbfd3720871daf
Dileep singh <rajsingh@Dileeps-MacBook-Pro.local>
2020-05-23 01:54:16 +0530

    complete till lect 94 it will be restricted user to comment

--- blog/views.py
+++ blog/views.py
@@ -18,7 +18,7 @@ def blogHome(request):
 def blogPost(request,slug):
     post = Post.objects.filter(slug=slug).first()
     comments =BlogComment.objects.filter(post=post)
-    context = {'post':post,'comments':comments}
+    context = {'post':post,'comments':comments,'user':request.user}
     return render(request,'blog/blogPost.html',context)
     # return HttpResponse(f'This is blog:{slug}')     
 
